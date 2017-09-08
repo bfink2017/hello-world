33aea926ecdaeb06f602226629a55d0559a11ffc
Brendan Fink <bfink@stu.jsu.edu>
2017-09-08 07:34:48 -0500

    CS310

--- Class2.java
+++ Class2.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Brendan Fink
  */
 public class Class2 {
     private void f1() {
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 /**
  *
- * @author Your Name
+ * @author Brendan Fink
  */
 public class HelloWorld {
     public static void main(String[] args) {
